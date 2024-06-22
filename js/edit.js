efaed31ecaecb5a44d1cabe6fd044683f758a408
Muhvinlah <fadillah.ervin@gmail.com>
2024-06-22 15:27:23 +0700

    update file terbaru

---
+++ js/edit.js
@@ -0,0 +1,70 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, set, get, update, remove, push, ref as databaseRef, child, onValue, orderByChild } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyAvOM6KwAcJBjj7LGOY9hlPditaJ3gAKnQ",
+   authDomain: "gcp-integration-425706.firebaseapp.com",
+   databaseURL: "https://gcp-integration-425706-default-rtdb.asia-southeast1.firebasedatabase.app",
+   projectId: "gcp-integration-425706",
+   storageBucket: "gcp-integration-425706.appspot.com",
+   messagingSenderId: "529433350922",
+   appId: "1:529433350922:web:4d18d1f2ff1dcabbf220f3",
+   measurementId: "G-WDEGEW6P0V"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const storage = getStorage(app);
+
+// PROSES MEMBUAT ARTIKEL
+document.addEventListener("DOMContentLoaded", () => {
+    // Mendapatkan elemen input
+    const articleTitleInput = document.querySelector("#title");
+    const articleContentInput = document.querySelector("#article");
+    const articleImageInput = document.querySelector("#newsBanner");
+
+    // Tombol submit
+    const createArticleBtn = document.querySelector("#publishBtn");
+ 
+    // Inisialisasi fungsi untuk membuat artikel
+    const createArticle = async () => {
+       // Mendapatkan file gambar yang diunggah
+       const imageFile = articleImageInput.files[0];
+       // Upload gambar ke Firebase Storage
+       const storageReference = storageRef(storage, 'Images/' + articleTitleInput);
+       await uploadBytes(storageReference, imageFile);
+ 
+       // Mendapatkan URL gambar yang diunggah
+       const imageURL = await getDownloadURL(storageReference);
+ 
+       // Jika input tidak terisi maka tampilkan pesan error 
+       if (!imageFile || !articleTitleInput.value || !articleContentInput.value) {
+          alert("Please fill out all fields");
+          return;
+       }
+ 
+       // Menyimpan data artikel ke Firebase Realtime Database
+       await set(push(databaseRef(db, "News")), {
+          ArticleTitle: articleTitleInput.value,
+          ArticleContent: articleContentInput.value,
+          ArticleImage: imageURL, // Menyimpan URL gambar
+          CreatedAt: new Date().toUTCString()
+       })
+          .then(() => {
+             // Menampilkan pesan sukses
+             alert("Article created successfully!");
+             window.location.reload();
+          })
+          .catch((error) => {
+             // Menampilkan pesan error
+             alert("Error creating article: " + error);
+          });
+    };
+ 
+    // Menjalankan fungsi createRecipe ketika tombol create recipe ditekan
+    createArticleBtn.addEventListener("click", (event) => {
+       event.preventDefault(); // Mencegah halaman untuk reload atau action default lainnya
+       createArticle();
+    });
+ });
---
+++ js/home.js
@@ -0,0 +1,67 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, set, get, update, remove, push, ref, child, onValue, orderByChild } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvOM6KwAcJBjj7LGOY9hlPditaJ3gAKnQ",
+  authDomain: "gcp-integration-425706.firebaseapp.com",
+  databaseURL: "https://gcp-integration-425706-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gcp-integration-425706",
+  storageBucket: "gcp-integration-425706.appspot.com",
+  messagingSenderId: "529433350922",
+  appId: "1:529433350922:web:4d18d1f2ff1dcabbf220f3",
+  measurementId: "G-WDEGEW6P0V"
+};
+
+const app = initializeApp(firebaseConfig);
+const dbRef = ref(getDatabase());
+
+get(child(dbRef, "News")).then((snapshot) => {
+    if (snapshot.exists()) {
+      const newsData = snapshot.val();
+      const newsContainer = document.getElementById('news');
+
+    // Iterate through each news item
+    Object.keys(newsData).forEach((key) => {
+      const { ArticleImage, ArticleTitle, CreatedAt, ArticleContent } = newsData[key];
+
+      // Create HTML elements for each news item
+      const articleDiv = document.createElement('article');
+      articleDiv.classList.add('p-md-5'); // Optional: Add a class for styling
+      articleDiv.classList.add('text-body-emphasis');
+      articleDiv.classList.add('border-end');
+
+      const imageElement = document.createElement('img');
+      imageElement.src = ArticleImage;
+      imageElement.alt = ArticleTitle; // Optional: Add alt text
+      imageElement.classList.add('img-fluid')
+
+      const titleElement = document.createElement('a');
+      titleElement.textContent = ArticleTitle;
+      titleElement.href = "berita.html";
+      titleElement.classList.add('nav-item');
+      titleElement.classList.add('nav-link');
+      titleElement.classList.add('link-body-emphasis');
+      titleElement.classList.add('fs-1');
+      titleElement.classList.add('fst-italic');
+      titleElement.classList.add('text-capitalize');
+
+      const dateElement = document.createElement('p');
+      dateElement.textContent = `Published on: ${CreatedAt}`;
+
+      const contentElement = document.createElement('p');
+      contentElement.textContent = ArticleContent;
+
+      // Append elements to the news container
+      articleDiv.appendChild(imageElement);
+      articleDiv.appendChild(titleElement);
+      articleDiv.appendChild(dateElement);
+      articleDiv.appendChild(contentElement);
+
+      newsContainer.appendChild(articleDiv);
+    });
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
